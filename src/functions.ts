20677f1089b629fcb6c20a89848d50a62235cd23
Le Huy <leduchuy2205@gmail.com>
2023-08-21 15:39:44 +0700

    Generate class by fileName

--- src/functions.ts
+++ src/functions.ts
@@ -1,64 +1,65 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import { lstatSync } from 'fs';
-import { dirname } from 'path';
+import { lstatSync } from "fs";
+import { dirname } from "path";
 
 function isDirectory(path: string): boolean {
-    return lstatSync(path).isDirectory();
+  return lstatSync(path).isDirectory();
 }
 
-export async function createClassCommand(extensionContext: vscode.ExtensionContext, context: any, disposable: vscode.Disposable) {
+export async function createClassCommand(
+  extensionContext: vscode.ExtensionContext,
+  context: any,
+  disposable: vscode.Disposable
+) {
+  // prompt for class name
+  let fileName = await vscode.window.showInputBox({
+    prompt: "Enter your file name",
+  });
+  if (fileName === undefined) {
+    console.log("No file name entered");
+    return;
+  }
 
-    // prompt for class name
-    let className = await vscode.window.showInputBox({ prompt: 'Enter class name' });
-    if (className === undefined) {
-        console.log('No class name entered');
-        return;
-    }
+  let className = "";
+  let fileNamePartitions = fileName.split("_");
 
-    console.log('Class name entered: ' + className);
+  for (let name in fileNamePartitions) {
+    className += name.charAt(0).toUpperCase() + name.slice(1);
+  }
 
-    let fileName = '';
+  console.log("Snake case: " + fileName);
 
-    // if class name is capital case, convert to snake case
-    if (className.match(/^[A-Z][a-z]+$/)) {
-        fileName = className.replace(/([A-Z])/g, '_$1').toLowerCase();
-        fileName = fileName.substring(1);
-    } else {
-        fileName = className.toLowerCase();
-    }
+  // get the path of the right clicked file or directory
 
-    console.log('Snake case: ' + fileName);
+  let dir =
+    context?.fsPath ?? vscode.window.activeTextEditor?.document.fileName;
+  if (dir === undefined) {
+    dir = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+  }
+  if (dir === undefined) {
+    console.log("No file path found");
+    return;
+  }
+  dir = dir.replace(/\\/g, "/");
 
-    // get the path of the right clicked file or directory
+  // if dir is a file, get the directory
+  if (!isDirectory(dir)) {
+    console.log("File path is a file: " + dir);
+    console.log("Getting directory");
+    dir = dirname(dir);
+    console.log("Directory: " + dir);
+  }
 
-    let dir = context?.fsPath ?? vscode.window.activeTextEditor?.document.fileName;
-    if (dir === undefined) {
-        dir = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-    }
-    if (dir === undefined) {
-        console.log('No file path found');
-        return;
-    }
-    dir = dir.replace(/\\/g, '/');
+  if (!dir.endsWith("/")) {
+    dir += "/";
+  }
 
-    // if dir is a file, get the directory
-    if (!isDirectory(dir)) {
-        console.log('File path is a file: ' + dir);
-        console.log('Getting directory');
-        dir = dirname(dir);
-        console.log('Directory: ' + dir);
-    }
+  console.log("Currently opened tab file path: " + dir);
+  let path = vscode.Uri.file(dir + fileName + ".dart");
+  console.log("File name: " + path.fsPath);
 
-    if (!dir.endsWith('/')) { 
-        dir += '/';
-    }
-
-    console.log('Currently opened tab file path: ' + dir);
-    let path = vscode.Uri.file(dir + fileName + '.dart');
-    console.log('File name: ' + path.fsPath);
-
-    let contents = `
+  let contents = `
 import 'package:json_annotation/json_annotation.dart';
 part '${fileName}.g.dart';
 
@@ -71,18 +72,16 @@ Map<String, dynamic> toJson() => _$${className}ToJson(this);
 }
     `;
 
-    // create a file in current directory
-    await vscode.workspace.fs.writeFile(path, Buffer.from(contents));
+  // create a file in current directory
+  await vscode.workspace.fs.writeFile(path, Buffer.from(contents));
 
-    // show file in editor
-    vscode.workspace.openTextDocument(path).then(doc => {
-        vscode.window.showTextDocument(doc);
-    });
+  // show file in editor
+  vscode.workspace.openTextDocument(path).then((doc) => {
+    vscode.window.showTextDocument(doc);
+  });
 
-    // show message
-    vscode.window.showInformationMessage(
-        `Created file: ${path.fsPath}`
-    );
+  // show message
+  vscode.window.showInformationMessage(`Created file: ${path.fsPath}`);
 
-    extensionContext.subscriptions.push();
+  extensionContext.subscriptions.push();
 }
